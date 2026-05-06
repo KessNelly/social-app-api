d0f1bd0c369206c416f459b84d8c4d725fc57ee5
KessNelly <ojkessnelly@gmail.com>
2026-05-06 23:26:10 +0100

    Post routes

---
+++ src/routes/postRoutes.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const {
+  createPost,
+  getPublishedPosts,
+  getSinglePost,
+  getMyPosts,
+  updatePost,
+  publishPost,
+  deletePost,
+} = require("../modules/posts/postController");
+
+const protect = require("../middleware/auth");
+
+const router = express.Router();
+
+// PUBLIC ROUTES
+router.get("/published", getPublishedPosts); // Get all published posts
+
+// PROTECTED ROUTES
+router.use(protect);
+
+router.post("/", createPost); // Create new post (draft)
+router.get("/me", getMyPosts); // Get user's own posts
+router.patch("/:id", updatePost); // Update post
+router.patch("/:id/publish", publishPost); // Publish a draft
+router.delete("/:id", deletePost); // Delete post
+
+router.get("/:id", getSinglePost); // Get single published post
+
+module.exports = router;
